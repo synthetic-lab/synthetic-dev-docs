20b42f13fcc5bc6d7ec3d505afa11003d5fdfd64
Billy Cao <the.billy.c@gmail.com>
2025-09-19 12:43:15 -0700

    Fix dev.synthetic.new mobile sidebar and width; make subdomain public (#707)
    * Allow non-logged-in users to access dev.
    
    * Fix middleware types
    
    * Fix sidebar insets wider than remaning container/viewport width
    
    * Add sidebar trigger for smaller viewports
    
    * Fix trigger button width

--- layout.tsx
+++ layout.tsx
@@ -1,11 +1,14 @@
+import css from "@/app/components/css";
 import { LargePageContainer } from "@/app/components/page/containers";
+
+export const DEV_PADDING_X = "px-8";
 
 export default async function DevLayout({
   children,
 }: Readonly<{
   children: React.ReactNode,
 }>) {
-  return <LargePageContainer className="px-8 py-4">
+  return <LargePageContainer className={css(DEV_PADDING_X, "py-4")}>
     {children}
   </LargePageContainer>;
 }
