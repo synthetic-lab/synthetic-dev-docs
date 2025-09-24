77b6ea10eb60126c8a4e895b6ab0c8667f997326
Billy Cao <the.billy.c@gmail.com>
2025-09-25 01:20:03 +0800

    Add tip to API docs noting endpoints that don't count against subscription limits (#721)
    * doc tweak
    
    * Add note to free endpoints that they're free
    
    * cleanup

---
+++ components/free-endpoint-tip-alert.tsx
@@ -0,0 +1,19 @@
+import { Alert, AlertTitle, AlertDescription } from "@/app/components/ui/alert";
+import { InfoIcon } from "lucide-react";
+import { SubdomainLink } from "@/app/components/subdomain-link";
+
+interface FreeEndpointTipAlertProps {
+  endpoint: string,
+}
+
+export function FreeEndpointTipAlert({ endpoint }: FreeEndpointTipAlertProps) {
+  return <Alert className="mb-4">
+    <InfoIcon color="#00bb00" />
+    <AlertTitle>Tip</AlertTitle>
+    <AlertDescription>
+      <p>
+        <strong className="font-bold">{endpoint}</strong> requests do not count against your <SubdomainLink href="/billing/subscriptions">subscription</SubdomainLink> limits!
+      </p>
+    </AlertDescription>
+  </Alert>;
+}
